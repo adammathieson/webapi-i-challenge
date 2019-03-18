92e397a3d0120fdd5405137faa2114e88e996eb4
Adam Mathieson <mathieson1222@gmail.com>
2019-03-18 13:36:31 -0700

    added code to implement the API requirements

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello from Express')
+})
+
+server.listen(5000, () => {
+    console.log('Server running on http://localhost:5000');
+});
