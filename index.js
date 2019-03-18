14adf1753f9d581ee972cbcb21ed54d6f80bda2c
Adam Mathieson <mathieson1222@gmail.com>
2019-03-18 15:31:40 -0700

    added .get and .post of CRUD

--- index.js
+++ index.js
@@ -6,8 +6,44 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send('Hello from Express')
+
+//Get
+server.get('/api/users', (req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error retrieving users' });
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+        db
+            .findById(id)
+            .then(users => {
+                res.status(200).json(users);
+            })
+            .catch(error => {
+                res.status(404).json({message: 'The user with the specified id does not exist' })
+            })
+})
+
+//Post
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+    console.log('user information', userInfo)
+
+    db
+        .insert(userInfo)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(error => {
+            res.status(400).json({ errorMessage: 'Please provide name and bio for the user' })
+        })
 })
 
 server.listen(5000, () => {
