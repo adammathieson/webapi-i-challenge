e52bcc03e41e68336d0728fde076866f7ce4f773
Adam Mathieson <mathieson1222@gmail.com>
2019-03-18 17:27:39 -0700

    added .put and .delete endpoints

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ server.get('/api/users', (req, res) => {
         });
 });
 
+
+//Get (user)
 server.get('/api/users/:id', (req, res) => {
     const id = req.params.id;
         db
@@ -35,14 +37,52 @@ server.get('/api/users/:id', (req, res) => {
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
     console.log('user information', userInfo)
-
+    
     db
         .insert(userInfo)
         .then(user => {
-            res.status(201).json(user);
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json
+            } else {
+                res.status(201).json(user);
+            }
+        
         })
         .catch(error => {
             res.status(400).json({ errorMessage: 'Please provide name and bio for the user' })
+        })
+})
+
+//Delete
+server.delete('/user/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+        .remove(id)
+        .then(deleted => {
+            res.status(204).end();
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error deleting user' })
+        })
+})
+
+//Update
+server.put('/user/:id', (req, res) => {
+    const id = req.body.id;
+    const changes = req.body;
+
+    db
+        .update(id, changes)
+        .then(updated => {
+            if (updated) {
+                res.status(200).json(updated);
+            } else {
+                res.status(404).json({ message: 'user not found' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error updating the user' })
         })
 })
 
